0c998e81e174c2ac5b8d5b65a5976fc658d5d12f
diaocheng <diaocheng@outlook.com>
2017-05-16 20:49:41 +0800

    添加.travis.yml集成构建

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import reducer from './reducer'
+import state from './store/state'
+import enhancer from './store/enhancer'
 import App from './App'
+
+const store = createStore(
+  reducer,
+  state,
+  enhancer
+)
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(<App store={store} />, div)
 })
